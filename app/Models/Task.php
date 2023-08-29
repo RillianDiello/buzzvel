a609eb4ad37025672c79a55a1a35cce954e5f6a7
Rillian Diello <rilliansf@gmail.com>
2023-08-29 16:48:13 -0400

    Buzzvel: Models Task, TaskStatus and User

---
+++ app/Models/Task.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    protected $fillable = ['title', 'description', 'user_id', 'task_status_id'];
+
+    const DONE = 1;
+    const PROGRESS = 2;
+    const WAITTING = 3;
+    const DELETED = 4;
+
+    use HasFactory;
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function taskStatus()
+    {
+        return $this->belongsTo(TaskStatus::class);
+    }
+}
